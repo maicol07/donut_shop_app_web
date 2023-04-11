4b7f52f388f2bb62a1b01f88986751f9e39f0a7f
Maicol Battistini <maicolbattistini@live.it>
2023-04-11 12:29:25 +0200

    perf: Change function to static

--- database/migrations/2023_04_11_121512_create_ingredients_table.php
+++ database/migrations/2023_04_11_121512_create_ingredients_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('ingredients', function (Blueprint $table) {
+        Schema::create('ingredients', static function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->boolean('allergen')->default(false);
