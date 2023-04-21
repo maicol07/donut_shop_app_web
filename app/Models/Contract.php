e2bf2458943c7730a9d3a2ce1a598e153ce9d781
Maicol Battistini <maicolbattistini@live.it>
2023-04-21 12:45:29 +0200

    perf: ⚡ Changed dates casting

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -18,8 +18,8 @@ class Contract extends Model
     ];
 
     protected $casts = [
-        'start_date' => 'datetime',
-        'end_date' => 'datetime',
+        'start_date' => 'date',
+        'end_date' => 'date',
     ];
 
     final public function shifts(): HasMany
