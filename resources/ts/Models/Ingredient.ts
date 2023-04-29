727e715bb09d7ec460c9dd68eb2f110f539debd5
Maicol Battistini <maicolbattistini@live.it>
2023-04-29 17:41:54 +0200

    feat: ✨ Added models

---
+++ resources/ts/Models/Ingredient.ts
@@ -0,0 +1,16 @@
+import Model, {
+  ModelAttributes,
+  ModelRelations
+} from '~/Models/Model';
+
+export interface IngredientAttributes extends ModelAttributes {
+  name: string;
+  allergen: boolean;
+}
+
+export interface IngredientRelations extends ModelRelations {
+  // notifications: DatabaseNotifications
+}
+
+export default class Ingredient extends Model<IngredientAttributes, IngredientRelations> {
+}
