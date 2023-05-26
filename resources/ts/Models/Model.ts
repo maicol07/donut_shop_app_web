455468ae5e474d41ea7f41ac6c99c1e70cab7f0a
Maicol Battistini <maicolbattistini@live.it>
2023-05-26 14:59:02 +0200

    feat: ✨ Allow to set pivots

--- resources/ts/Models/Model.ts
+++ resources/ts/Models/Model.ts
@@ -3,9 +3,10 @@ import {
   PaginationStrategy,
   PluralResponse
 } from 'coloquent';
-import type {ValueOf} from 'type-fest';
+import type {Constructor, ValueOf} from 'type-fest';
 
 import RequestHttpClient from '~/Models/Http/RequestHttpClient';
+import {Resource} from 'coloquent/dist/Resource';
 
 export interface ModelAttributes {
   id: number;
@@ -19,13 +20,19 @@ export interface ModelRelations {
   [key: string]: unknown;
 }
 
+export interface ModelRelationsPivots extends Record<string, Record<string, unknown>> {
+}
+
 /**
  * The base model for all models.
  */
-export default abstract class Model<A extends ModelAttributes, R extends ModelRelations> extends BaseModel {
+// @ts-expect-error – Necessary for overriding serialize
+export default abstract class Model<A extends ModelAttributes, R extends ModelRelations = ModelRelations, RP extends ModelRelationsPivots = ModelRelationsPivots> extends BaseModel {
   protected static paginationStrategy = PaginationStrategy.PageBased;
   protected static jsonApiBaseUrl = '/api';
   protected static httpClient = new RequestHttpClient();
+  // @ts-ignore
+  private relationsPivots: RP = {};
 
   abstract attributesNames: (keyof A)[];
   __relations!: (keyof R)[];
@@ -42,7 +49,8 @@ export default abstract class Model<A extends ModelAttributes, R extends ModelRe
   /**
    * Returns all the instances of the model. (Alias of {@link Model.get}).
    */
-  static all<M extends Model<any, any>>(): Promise<PluralResponse<M>> {
+  // @ts-ignore
+  static all<M extends Model<any, any>>(this: Constructor<M>): Promise<PluralResponse<M>> {
     // @ts-expect-error
     return this.get();
   }
@@ -77,7 +85,36 @@ export default abstract class Model<A extends ModelAttributes, R extends ModelRe
   }
 
   getRelation<RN extends keyof R = keyof R>(relationName: RN) {
-    return super.getRelation(relationName as string) as ValueOf<R, RN>;
+    return super.getRelation(relationName as string) as ValueOf<R, RN> | ValueOf<R, RN>[];
+  }
+
+  setRelation<RN extends keyof R = keyof R>(relationName: RN, value: ValueOf<R, RN> | ValueOf<R, RN>[]) {
+    super.setRelation(relationName as string, value);
+  }
+
+  getRelationPivotData<RPN extends keyof RP = keyof RP>(relationName: RPN): ValueOf<RP, RPN> {
+    return this.relationsPivots[relationName];
+  }
+
+  setRelationPivotData<RPN extends keyof RP = keyof RP>(relationName: RPN, value: ValueOf<RP, RPN>) {
+    this.relationsPivots[relationName] = value;
+  }
+
+  private serialize() {
+    // @ts-expect-error - Serialize is private
+    const data: {data: Resource & {attributes: ModelAttributes; relationships: Record<string, {data: { type: string; id: string; attributes: Record<string, unknown>, pivots: Record<string, unknown> }}>}} = super.serialize();
+    for (const relationName of Object.keys(this.relationsPivots)) {
+      data.data.relationships[relationName].data.pivots = this.relationsPivots[relationName];
+    }
+    return data;
+  }
+
+  populateFromResource(resource: Resource) {
+    super.populateFromResource(resource);
+
+    for (const relationName of Object.keys(resource.relationships)) {
+      this.setRelationPivotData(relationName, resource.relationships[relationName].data.pivots);
+    }
   }
 
   /**
