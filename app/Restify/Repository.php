d2922feea7e9f10f6fe115b22f47cb0340b6754b
Maicol Battistini <maicolbattistini@live.it>
2023-06-13 11:29:38 +0200

    feat: ✨ Improved Restify repository

--- app/Restify/Repository.php
+++ app/Restify/Repository.php
@@ -11,11 +11,10 @@ use Binaryk\LaravelRestify\Http\Requests\RepositoryAttachRequest;
 use Binaryk\LaravelRestify\Http\Requests\RepositorySyncRequest;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use Binaryk\LaravelRestify\Repositories\Repository as RestifyRepository;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use Nette\Utils\Json;
@@ -95,6 +94,8 @@ abstract class Repository extends RestifyRepository
 
     /**
      * Adapt JSON:API request to Restify request.
+     *
+     * @psalm-suppress all
      */
     protected function adaptJsonApiRequest(RestifyRequest $request, bool $snake_attributes = false): void
     {
@@ -114,44 +115,62 @@ abstract class Repository extends RestifyRepository
          * @type array<string, string|int>|array<string, array<string, array<string|int, array<string>>> $relationship
          */
         $relationships = array_map(
-            /**
-             * @param array{
-             *     data: array{type: string, id: int}|array{type: string, id: int}[]
-             * } $relationship
-             */
+        /**
+         * @param array{
+         *     data: array{type: string, id: int}|array{type: string, id: int}[]
+         * } $relationship
+         */
             static fn (array $relationship): int|array => Arr::get($relationship, 'data.id') ?? Arr::pluck($relationship['data'], 'pivots', 'id'),
             $relationships
         );
+        $routes = Route::getRoutes()->getRoutesByMethod()['POST'];
         $current_route = Route::current();
-        $attach_route = Route::getRoutes()->getRoutesByMethod()['POST']['api/{repository}/{repositoryId}/attach/{relatedRepository}'];
+        $base = trim(config('restify.base'), '/');
+        $attach_route = $routes[$base.'/{repository}/{repositoryId}/attach/{relatedRepository}'];
         assert($attach_route instanceof \Illuminate\Routing\Route);
-        $sync_route = Route::getRoutes()->getRoutesByMethod()['POST']['api/{repository}/{repositoryId}/sync/{relatedRepository}'];
+        $sync_route = $routes[$base.'/{repository}/{repositoryId}/sync/{relatedRepository}'];
         assert($sync_route instanceof \Illuminate\Routing\Route);
 
         foreach ($relationships as $relation_name => $pivots) {
-            // Use sync request to remove current relationships
-            $sync_body = [$relation_name => []];
-            $sync_request = RepositorySyncRequest::create("{$request->getRequestUri()}/sync/$relation_name", 'POST', $sync_body, $request->cookie(), $request->allFiles(), $request->server(), Json::encode($sync_body));
-            $sync_route->bind($sync_request);
-            foreach ($current_route->parameters() as $name => $value) {
-                $sync_route->setParameter($name, $value);
-            }
-            $sync_route->setParameter('relatedRepository', $relation_name);
-            $sync_request->setRouteResolver(static fn () => $sync_route);
-            $response = app(RepositorySyncController::class)($sync_request);
-            if (!$response->isSuccessful()) {
-                exit($response->send());
-            }
-
             if (is_array($pivots)) {
+                // Use sync request to remove current relationships
+                /** @psalm-suppress InvalidArrayOffset */
+                $sync_body = [$relation_name => []];
+                $sync_request = RepositorySyncRequest::create(
+                    "{$request->getRequestUri()}/sync/$relation_name",
+                    'POST',
+                    $sync_body,
+                    $request->cookie(),
+                    $request->allFiles(),
+                    $request->server(),
+                    Json::encode($sync_body)
+                );
+                $sync_route->bind($sync_request);
+                foreach ($current_route->parameters() as $name => $value) {
+                    $sync_route->setParameter($name, $value);
+                }
+                $sync_route->setParameter('relatedRepository', $relation_name);
+                $sync_request->setRouteResolver(static fn () => $sync_route);
+                $response = app(RepositorySyncController::class)($sync_request);
+                if (!$response->isSuccessful()) {
+                    exit($response->send());
+                }
                 // Attach request relationships
                 $attach_bodies = Arr::mapWithKeys($pivots, static fn ($pivots, $id) => [
                     $id => [
-                    $relation_name => [$id],
-                    ...($pivots ?? [])
-                ]]);
+                        $relation_name => [$id],
+                        ...($pivots ?? []),
+                    ]]);
                 foreach ($attach_bodies as $attach_body) {
-                    $attach_request = RepositoryAttachRequest::create("{$request->getRequestUri()}/attach/$relation_name", 'POST', $attach_body, $request->cookie(), $request->allFiles(), $request->server(), Json::encode($attach_body));
+                    $attach_request = RepositoryAttachRequest::create(
+                        "{$request->getRequestUri()}/attach/$relation_name",
+                        'POST',
+                        $attach_body,
+                        $request->cookie(),
+                        $request->allFiles(),
+                        $request->server(),
+                        Json::encode($attach_body)
+                    );
                     $attach_route->bind($attach_request);
                     foreach ($current_route->parameters() as $name => $value) {
                         $attach_route->setParameter($name, $value);
@@ -172,8 +191,76 @@ abstract class Repository extends RestifyRepository
         ]);
     }
 
+    /**
+     * Return a list with relationship for the current model.
+     *
+     * @param RestifyRequest $request
+     */
     public function resolveRelationships($request): array
     {
-        return array_map(static fn ($data) => ['data' => $data], parent::resolveRelationships($request));
+        return Arr::whereNotNull(Arr::map(parent::resolveRelationships($request), static function (self|Collection|null $repository) use ($request) {
+            if ($repository instanceof self) {
+                return [
+                    'data' => [
+                        'id' => $repository->getId($request),
+                        'type' => $repository->getType($request),
+                    ],
+                    'included' => $repository,
+                ];
+            }
+
+            if ($repository instanceof Collection && $repository->isNotEmpty()) {
+                return [
+                    'data' => $repository->map(static fn (self $repository) => [
+                        'id' => $repository->getId($request),
+                        'type' => $repository->getType($request),
+                    ]),
+                    'included' => $repository
+                ];
+            }
+
+            return null;
+        }));
+    }
+
+    public function index(RestifyRequest $request): JsonResponse
+    {
+        $response = parent::index($request);
+        $data = $response->getData(true);
+
+        $included = Arr::collapse(Arr::pluck($data['data'], 'relationships.*.included'));
+
+        $included = array_filter($included);
+        $data['included'] = Arr::collapse($included);
+
+        // Remove included from relationships (we already have them in included)
+        foreach ($data['data'] as &$item) {
+            if (isset($item['relationships'])) {
+                foreach ($item['relationships'] as &$relationship) {
+                    Arr::forget($relationship, 'included');
+                }
+            }
+        }
+
+        return $response->setData($data);
+    }
+
+    public function show(RestifyRequest $request, $repositoryId): JsonResponse
+    {
+        $response = parent::show($request, $repositoryId);
+        $data = $response->getData(true);
+
+        $rel = Arr::get($data, 'data.relationships');
+
+        if ($rel) {
+            $included = Arr::pluck($rel, 'included');
+
+            /**
+             * @return array|RestifyRepository|Collection|null
+             */
+            $data['included'] = array_filter($included);
+        }
+
+        return $response->setData($data);
     }
 }
