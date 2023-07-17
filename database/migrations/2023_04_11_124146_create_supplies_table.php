eb6e4e53ee07224b6a176694330a7d65fcb3f23a
Maicol Battistini <maicolbattistini@live.it>
2023-07-17 14:30:28 +0200

    refactor: ♻️ Code reformat

--- database/migrations/2023_04_11_124146_create_supplies_table.php
+++ database/migrations/2023_04_11_124146_create_supplies_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('supplies', static function (Blueprint $table) {
             $table->id();
             $table->dateTime('start_date');
             $table->dateTime('end_date');
-            $table->string("company_vat_number");
-            $table->foreign("company_vat_number")->references("vat_number")->on("companies");
+            $table->string('company_vat_number');
+            $table->foreign('company_vat_number')->references('vat_number')->on('companies');
             $table->timestamps();
         });
     }
