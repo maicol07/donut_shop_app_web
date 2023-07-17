a592540fcd642fc3e84bb8275331e7ddd6bf8835
Maicol Battistini <maicolbattistini@live.it>
2023-07-17 14:26:54 +0200

    fix: Added migration for missing constrained columns

---
+++ database/migrations/2023_07_15_133619_add_missing_relationships.php
@@ -0,0 +1,156 @@
+<?php
+
+use App\Models\Account;
+use App\Models\Contract;
+use App\Models\Customer;
+use App\Models\Discount;
+use App\Models\Donut;
+use App\Models\Employee;
+use App\Models\Ingredient;
+use App\Models\Sale;
+use App\Models\Shop;
+use App\Models\Supply;
+use App\Models\Warehouse;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('accounts', static function (Blueprint $table) {
+            $table->foreign('fiscal_code')->references(app(Customer::class)->getKeyName())->on(app(Customer::class)->getTable())->cascadeOnDelete();
+        });
+
+        Schema::table('availabilities', static function (Blueprint $table) {
+            $table->foreignIdFor(Donut::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Shop::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('compositions', static function (Blueprint $table) {
+            $table->foreignIdFor(Donut::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Ingredient::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('daily_reservations', static function (Blueprint $table) {
+            $table->foreignIdFor(Donut::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Supply::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('employee_assignments', static function (Blueprint $table) {
+            $table->foreignIdFor(Contract::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreign('fiscal_code')->references('fiscal_code')->on(app(Employee::class)->getTable())->cascadeOnDelete();
+            $table->foreignIdFor(Shop::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('online_sales', static function (Blueprint $table) {
+            $table->foreignIdFor(Sale::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreign('username')->references(app(Account::class)->getKeyName())->on(app(Account::class)->getTable())->cascadeOnDelete();
+        });
+
+        Schema::table('purchases', static function (Blueprint $table) {
+            $table->foreignIdFor(Sale::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Donut::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('sales', static function (Blueprint $table) {
+            $table->foreignIdFor(Shop::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Supply::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('shifts', static function (Blueprint $table) {
+            $table->foreignIdFor(Contract::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('shop_sales', static function (Blueprint $table) {
+            $table->foreignIdFor(Sale::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('stocks', static function (Blueprint $table) {
+            $table->foreignIdFor(Warehouse::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Shop::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('storages', static function (Blueprint $table) {
+            $table->foreignIdFor(Warehouse::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Ingredient::class)->change()->constrained()->cascadeOnDelete();
+        });
+
+        Schema::table('tariffs', static function (Blueprint $table) {
+            $table->foreignIdFor(Discount::class)->change()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Donut::class)->change()->constrained()->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('accounts', static function (Blueprint $table) {
+            $table->dropForeign(['fiscal_code']);
+        });
+
+        Schema::table('availabilities', static function (Blueprint $table) {
+            $table->dropForeign(['donut_id']);
+            $table->dropForeign(['shop_id']);
+        });
+
+        Schema::table('compositions', static function (Blueprint $table) {
+            $table->dropForeign(['donut_id']);
+            $table->dropForeign(['ingredient_id']);
+        });
+
+        Schema::table('daily_reservations', static function (Blueprint $table) {
+            $table->dropForeign(['donut_id']);
+            $table->dropForeign(['supply_id']);
+        });
+
+        Schema::table('employee_assignments', static function (Blueprint $table) {
+            $table->dropForeign(['contract_id']);
+            $table->dropForeign(['fiscal_code']);
+            $table->dropForeign(['shop_id']);
+        });
+
+        Schema::table('online_sales', static function (Blueprint $table) {
+            $table->dropForeign(['sale_id']);
+            $table->dropForeign(['fiscal_code']);
+        });
+
+        Schema::table('purchases', static function (Blueprint $table) {
+            $table->dropForeign(['sale_id']);
+            $table->dropForeign(['donut_id']);
+        });
+
+        Schema::table('sales', static function (Blueprint $table) {
+            $table->dropForeign(['shop_id']);
+            $table->dropForeign(['supply_id']);
+        });
+
+        Schema::table('shifts', static function (Blueprint $table) {
+            $table->dropForeign(['contract_id']);
+        });
+
+        Schema::table('shop_sales', static function (Blueprint $table) {
+            $table->dropForeign(['sale_id']);
+            $table->dropForeign(['shop_id']);
+        });
+
+        Schema::table('stocks', static function (Blueprint $table) {
+            $table->dropForeign(['warehouse_id']);
+            $table->dropForeign(['shop_id']);
+        });
+
+        Schema::table('storages', static function (Blueprint $table) {
+            $table->dropForeign(['warehouse_id']);
+            $table->dropForeign(['ingredient_id']);
+        });
+
+        Schema::table('supplies', static function (Blueprint $table) {
+            $table->dropForeign(['company_id']);
+        });
+
+        Schema::table('tariffs', static function (Blueprint $table) {
+            $table->dropForeign(['discount_id']);
+            $table->dropForeign(['donut_id']);
+        });
+    }
+};
